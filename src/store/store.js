0719a8897f6bb780208393e7f468b00bc002dd87
Fernando <fernandomercado529@gmail.com>
2022-06-22 12:26:52 -0400

    add store

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { counterSlice } from './slices/counter';
+
+
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer
+    }
+})
