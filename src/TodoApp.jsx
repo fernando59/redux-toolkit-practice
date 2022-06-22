d1680eb77423e696e999b7be8eeb5c0b030d068a
Fernando <fernandomercado529@gmail.com>
2022-06-22 14:15:57 -0400

    add get todo by id

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,15 @@
-import { useGetTodosQuery } from './store/apis/'
+import { useState } from 'react'
+import { useGetTodoByIdQuery, useGetTodosQuery } from './store/apis/'
 export const TodoApp = () => {
+  const [count, setCount] = useState(1)
   const {data:todos=[],isLoading} =useGetTodosQuery()
+  const {data:todo=[],isLoading:isLoadingId} =useGetTodoByIdQuery(count)
   return (
   <>
     <h1>Todos -RTK Query</h1>
     <hr />
 
-    <h4>{isLoading?'Cargando':''}</h4>
+    {/* <h4>{isLoading?'Cargando':''}</h4>
     <pre>...</pre>
     <ul>
       {
@@ -14,11 +17,13 @@ export const TodoApp = () => {
           <li key={todo.id}><strong>{todo.completed?'Done':'Pending'}</strong>{todo.title} </li>
         ))
       }
-    </ul>
+    </ul> */}
 
-    <button>
-        Next Todo
-    </button>
+
+    <pre>{JSON.stringify(todo)}</pre>
+
+    <button onClick={()=>setCount(count-1)}> Prev Todo </button>
+    <button onClick={()=>setCount(count+1)}> Next Todo </button>
   </>
   )
 }
