0bc9de9b6f21758d9f27e44f61415ecb3b1d8c1c
Fernando <fernandomercado529@gmail.com>
2022-06-22 13:21:35 -0400

    add pokemon slice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+export const PokemonApp = () => {
+    return (
+        <>
+            <div>PokemonApp</div>
+            <hr />
+            <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </>
+    )
+}
